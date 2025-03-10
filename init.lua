cf57436b5a6d188a18c95b6ad76137ae099b39ad
nick <nick@fortyau.com>
2025-03-10 15:07:30 -0400

    Customize startup screen

--- init.lua
+++ init.lua
@@ -255,17 +255,34 @@ require('lazy').setup({
   'tpope/vim-fugitive',
   'sodapopcan/vim-twiggy',
   'christoomey/vim-tmux-navigator',
+  -- {
+  --   'mfussenegger/nvim-jdtls',
+  --   config = function()
+  --     require('lspconfig').jdtls.setup { cmd = { 'jdtls' } }
+  --   end,
+  -- },
   'edkolev/tmuxline.vim',
   {
     'goolord/alpha-nvim',
     -- dependencies = { 'echasnovski/mini.icons' },
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-      local startify = require 'alpha.themes.startify'
-      -- available: devicons, mini, default is mini
-      -- if provider not loaded and enabled is true, it will try to use another provider
-      startify.file_icons.provider = 'devicons'
-      require('alpha').setup(startify.config)
+      local dashboard_theme = require 'alpha.themes.theta'
+      dashboard_theme.header.val = {
+        '                                                     ',
+        '  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ',
+        '  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ',
+        '  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ',
+        '  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ',
+        '  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ',
+        '  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ',
+        '                                                     ',
+      }
+
+      table.insert(dashboard_theme.config.layout, 1, { type = 'padding', val = 10 }) -- add more top padding
+      table.remove(dashboard_theme.config.layout, 7) -- remove quick links
+      dashboard_theme.file_icons.provider = 'devicons'
+      require('alpha').setup(dashboard_theme.config)
     end,
   },
   's1n7ax/nvim-window-picker',
@@ -702,6 +719,8 @@ require('lazy').setup({
             python = {
               analysis = {
                 typeCheckingMode = 'off',
+                autoSearchPaths = true,
+                useLibraryCodeForTypes = true,
               },
             },
           },
